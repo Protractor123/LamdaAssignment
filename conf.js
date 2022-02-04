23bf7c81607e0805ebf6d776d10ce4fcc4981e05
Protractor123 <ervivek.kumar08@gmail.com>
2022-02-04 14:28:08 +0000

    update conf.js

--- conf.js
+++ conf.js
@@ -3,9 +3,9 @@ exports.config = {
     framework: 'jasmine',
    specs: ['./*spec.js'],    
     directConnect: true,
-    chromeDriver:
-        'C:/Users/vkumar10/AppData/Roaming/npm/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_73.0.3683.75.exe',
-
+    // chromeDriver:
+    //     'C:/Users/vkumar10/AppData/Roaming/npm/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_73.0.3683.75.exe',
+    directConnect:true,
 onPrepare: async () => {     
         await browser.driver
             .manage()
